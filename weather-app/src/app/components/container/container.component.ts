9e5acfa20f6208e31f1ac1f1ada7d4fb506e22eb
4AK1LLA <ruslan.tretyakov2003@gmail.com>
2022-12-11 23:18:26 +0200

    Implemented search component

--- weather-app/src/app/components/container/container.component.ts
+++ weather-app/src/app/components/container/container.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { WeatherDTO } from 'src/app/models/weather.dto';
 import { WeatherService } from 'src/app/services/weather.service';
 
@@ -7,29 +7,25 @@ import { WeatherService } from 'src/app/services/weather.service';
   templateUrl: './container.component.html',
   styleUrls: ['./container.component.css']
 })
-export class ContainerComponent implements OnInit {
+export class ContainerComponent {
 
-  WeatherDTO?: WeatherDTO;
-  CityName: string = 'New York';
+  weatherDTO?: WeatherDTO;
 
   constructor(private weatherService: WeatherService) { }
 
-  ngOnInit(): void {
-
+  onInputSubmit(cityName: string): void {
     this.weatherService
-      .getWeatherDataObservable(this.CityName).subscribe({
+      .getWeatherDataObservable(cityName).subscribe({
         next: (response) => {
-          this.WeatherDTO = {
+          this.weatherDTO = {
             temperature: response.temp,
             min_temperature: response.min_temp,
             max_temperature: response.max_temp,
             humidity: response.humidity,
             wind_speed: response.wind_speed,
-            city_name: this.CityName
+            city_name: cityName
           }
         }
       });
-
   }
-
 }
